89a1be9fc1dd7c17863ea7bfb2f4cc8f86808171
Igor <nimgrin@gmail.com>
2020-05-05 10:26:22 +0300

    changed path in publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/';
+  const publicPath = 'https://github.com/Nimgr/spa.git';
 
   const pcss = {
     test: /\.(p|post|)css$/,
