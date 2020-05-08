dc11180345ab89b1267d2ec8d2db9d1191e97c9a
Igor <nimgrin@gmail.com>
2020-05-08 10:49:43 +0300

    made changes in according to comments for 1st week homework + added parallax

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = 'https://github.com/Nimgr/spa.git';
+  const publicPath = 'https://nimgr/github.io/spa/dist/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
